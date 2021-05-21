32061bb73131a4dd3dd4f581bca913f3c77b5e52
Wei Shi <weishi9999@gmail.com>
2021-05-20 19:39:20 -0700

    update for 2020-09-01-hybrid profile

--- index.js
+++ index.js
@@ -2,38 +2,45 @@
 // process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'; 
 
 var Environment = require("@azure/ms-rest-azure-env");
-var msRestNodeAuth = require('@azure/ms-rest-nodeauth');
-var KeyVaultManagementClient = require("@azure/arm-keyvault-profile-2019-03-01-hybrid").KeyVaultManagementClient;
-var ResourceManagementClient = require('@azure/arm-resources-profile-hybrid-2019-03-01').ResourceManagementClient;
+var msRestNodeAuth = require("@azure/ms-rest-nodeauth");
+var KeyVaultManagementClient = require("@azure/arm-keyvault-profile-2020-09-01-hybrid").KeyVaultManagementClient;
+var ResourceManagementClient = require("@azure/arm-resources-profile-2020-09-01-hybrid").ResourceManagementClient;
 
-var util = require('util');
-const request = require('request');
-const requestPromise = util.promisify(request);
+var util = require("util");
+const request = require("request");
 
-// update these
+const spAppIdEnvName = "AZURE_SP_APP_ID";
+const spObjectIdEnvName = "AZURE_SP_APP_OBJECT_ID";
+const spSecretEnvName = "AZURE_SP_APP_SECRET";
+const tenantIdEnvName = "AZURE_TENANT_ID";
+const subscriptionIdEnvName = "AZURE_SUBSCRIPTION_ID"
+const armEndpointEnvName = "AZURE_ARM_ENDPOINT";
+const locationEnvName = "AZURE_LOCATION";
+
 validateEnvironmentVariables();
-var clientAppId = process.env['CLIENT_APP_ID'];
-var clientObjectId = process.env['CLIENT_OBJECT_ID'];
-var clientSecret = process.env['CLIENT_SECRET'];
-var tenantId = process.env['TENANT_ID'];
-var subscriptionId = process.env['SUBSCRIPTION_ID'];
-var armEndpoint = process.env['ARM_ENDPOINT'];
-var location = process.env['LOCATION'];
+
+var clientAppId = process.env[spAppIdEnvName];
+var clientObjectId = process.env[spObjectIdEnvName];
+var clientSecret = process.env[spSecretEnvName];
+var tenantId = process.env[tenantIdEnvName];
+var subscriptionId = process.env[subscriptionIdEnvName];
+var armEndpoint = process.env[armEndpointEnvName];
+var location = process.env[locationEnvName];
 var domain = tenantId;
-var resourceGroup = "azs-sample-rg";
-var keyVaultName = "azs-sample-kv";
-var secretName = "azs-app-created-secret";
-var secretValue = "azs-app-created-password";
+var resourceGroupName = "azure-sample-rg";
+var keyVaultName = "azure-sample-kv";
+var secretName = "azure-app-created-secret";
+var secretValue = "azure-app-created-password";
 
 function validateEnvironmentVariables() {
     var envs = [];
-    if (!process.env['CLIENT_APP_ID']) envs.push('CLIENT_APP_ID');
-    if (!process.env['CLIENT_OBJECT_ID']) envs.push('CLIENT_OBJECT_ID');
-    if (!process.env['CLIENT_SECRET']) envs.push('CLIENT_SECRET');
-    if (!process.env['TENANT_ID']) envs.push('TENANT_ID');
-    if (!process.env['SUBSCRIPTION_ID']) envs.push('SUBSCRIPTION_ID');
-    if (!process.env['ARM_ENDPOINT']) envs.push('ARM_ENDPOINT');
-    if (!process.env['LOCATION']) envs.push('LOCATION');
+    if (!process.env[spAppIdEnvName]) envs.push(spAppIdEnvName);
+    if (!process.env[spObjectIdEnvName]) envs.push(spObjectIdEnvName);
+    if (!process.env[spSecretEnvName]) envs.push(spSecretEnvName);
+    if (!process.env[tenantIdEnvName]) envs.push(tenantIdEnvName);
+    if (!process.env[subscriptionIdEnvName]) envs.push(subscriptionIdEnvName);
+    if (!process.env[armEndpointEnvName]) envs.push(armEndpointEnvName);
+    if (!process.env[locationEnvName]) envs.push(locationEnvName);
     if (envs.length > 0) {
         throw new Error(util.format('please set/export the following environment variables: %s', envs.toString()));
     }
@@ -47,7 +54,6 @@ function fetchEndpointMetadata() {
         "headers": { "User-Agent": "request" },
         "rejectUnauthorized": false
     };
-    // Return new promise 
     return new Promise(function (resolve, reject) {
         // Do async job
         request.get(options, function (err, resp, body) {
@@ -80,7 +86,7 @@ function setEnvironment(armEndpointMetadata) {
     options["environment"] = Environment.Environment.AzureStack;
     options["tokenAudience"] = map["activeDirectoryResourceId"];
 
-    var isAdfs = armEndpointMetadata.authentication.loginEndpoint.endsWith('adfs')
+    var isAdfs = armEndpointMetadata.authentication.loginEndpoint.endsWith('adfs') || armEndpointMetadata.authentication.loginEndpoint.endsWith('adfs/')
     if (isAdfs) {
         domain = 'adfs'
         options.environment.validateAuthority = false
@@ -96,17 +102,14 @@ function loginWithSP(envOptions) {
 }
 
 function createResourceGroup(credentials) {
-    var clientOptions = { "baseUri": armEndpoint };
-    var resourceClient = new ResourceManagementClient(credentials, subscriptionId, clientOptions);
+    var resourceClient = new ResourceManagementClient(credentials, subscriptionId);
     var parameters = { "location": location };
-    // Create sample resource group. 
-    console.log("Creating resource group: " + resourceGroup);
-    return resourceClient.resourceGroups.createOrUpdate(resourceGroup, parameters);
+    console.log("Creating resource group: " + resourceGroupName);
+    return resourceClient.resourceGroups.createOrUpdate(resourceGroupName, parameters);
 }
 
 function createKeyVault(credentials) {
-    var clientOptions = { "baseUri": armEndpoint };
-    var keyVaultClient = new KeyVaultManagementClient(credentials, subscriptionId, clientOptions);
+    var keyVaultClient = new KeyVaultManagementClient(credentials, subscriptionId);
     var parameters = {
         "location": location,
         "properties": {
@@ -125,12 +128,11 @@ function createKeyVault(credentials) {
     }; 
     console.log("Creating keyvault: " + keyVaultName);  
     // Create the sample key vault using the KV management client.
-    return keyVaultClient.vaults.createOrUpdate(resourceGroup, keyVaultName, parameters);
+    return keyVaultClient.vaults.createOrUpdate(resourceGroupName, keyVaultName, parameters);
 }
 
 function updateSecret(credentials) {
-    var clientOptions = { "baseUri": armEndpoint };
-    var keyVaultClient = new KeyVaultManagementClient(credentials, subscriptionId, clientOptions);
+    var keyVaultClient = new KeyVaultManagementClient(credentials, subscriptionId);
     var parameters = {
         "properties": {
             "attributes": {},
@@ -142,7 +144,7 @@ function updateSecret(credentials) {
         "tags": {}
     };
     console.log("Updating secret: " + secretName);
-    keyVaultClient.secrets.createOrUpdate(resourceGroup, keyVaultName, secretName, parameters, function (err, result) {
+    keyVaultClient.secrets.createOrUpdate(resourceGroupName, keyVaultName, secretName, parameters, function (err, result) {
         if (err) {
             console.log("Error while writing secret");
             console.log(err);
@@ -150,6 +152,7 @@ function updateSecret(credentials) {
             console.log("Secret set successfully");
             console.log(result);
         }
+        console.log(util.format('Please execute the following script for cleanup:\nnode cleanup.js %s %s', resourceGroupName, keyVaultName));
     });
 }
 
